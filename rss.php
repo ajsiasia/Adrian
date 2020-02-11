e291b6372ea540b31f7dc15c7afce3b07f41b41b
ajsiasia <60340841+ajsiasia@users.noreply.github.com>
2020-02-11 14:32:25 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,19 @@
+<?php
+$domOBJ = new DOMDocument();
+$domOBJ->load("https://app-pua.herokuapp.com/");
+
+$content = $domOBJ->getElementsByTagName("item");
+
+foreach ($content as $data) {
+
+    $username = $data->getElementsByTagName("username")->item(0)->nodeValue;
+    $email = $data->getElementsByTagName("email")->item(0)->nodeValue;
+    $address = $data->getElementsByTagName("address")->item(0)->nodeValue;
+
+    echo "
+    <ul>
+    <li>Username: <strong>$username</strong></li>
+    <li>Email: <strong>$email</strong></li>
+    <li>Address: <strong>$address</strong></li>
+    </ul>";
+}
