56d84eeabcd6ba8ef283eb0f72686e3a907bf7d6
ajsiasia <60340841+ajsiasia@users.noreply.github.com>
2020-02-11 14:31:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
-header ("Content-type: text/xml");
+header("Content-type: text/xml");
 
 $db_username = 'admin';
 $db_password = 'root1234';
 $db_hostname = 'dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com';
 $db_port = '3306';
-$db_name = 'db_1821599';
+$db_name = 'db_1822199';
 
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
-$q = "SELECT * FROM information";
+$q = "SELECT * FROM Table1";
 $r = mysqli_query($conn, $q);
 
-header("Content-type: text/xml");
-echo "<?xml version='1.0' encoding='UTF-8'?>";
+echo "<?xml version='1.0' encoding='UTF-8'?>
+<data>";
+
 while ($item = mysqli_fetch_object($r)) {
-	echo "<item>
-	<firstname>$item->firstname</firstname>
-	<lastname>$item->lastname</lastname>
-	<address>$item->address</address>
-	<contacts>$item->contacts</contacts>
-	</item>
-	";
+    echo "<item>
+    <username>$item->username</username>
+    <email>$item->email</email>
+    <address>$item->address</address>
+    </item>";
 }
+
+echo "</data>";
